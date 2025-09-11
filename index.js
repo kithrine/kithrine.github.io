12f40c85eb20a2f8dc48f67ab7197ab3d57f2cd3
Katherine Tensfeldt <ktensfeldt@perseverenow.org>
2025-09-11 14:49:36 -0700

    Adds navbar, and tabs and input with buttons and the basic outline of the list of tasks. Begins to implement localStorage.

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
-// Load any existing todos
+// Get elements by their ID's
+const addTaskButton = document.getElementById("addTaskButton")
+const addTaskText = document.getElementById("addTaskText")
+const listContainer = document.getElementById("listContainer")
 
-// If there are any todos, show them on the page
+// Initialize todo data, { id: 0, task: "todo" }
+const tasks = []
+const completedTasks = []
 
-// 
+// Add click event listener to task add button
+const addTask = () => {
+  console.log("addTaskButton click", addTaskText.value)
+  // Create task record
+  const newTask = { id: tasks.length + 1, text: addTaskText.value, status: "Active", priority: "Normal" }
+  tasks.push(newTask)
+  console.log("newTask", newTask, "tasks", tasks)
+  // Clear existing HTML
+
+
+  // Populate HTML w/ template literal
+  const newTaskHTML = `
+    <li id="${newTask.id}" class="list-row">
+      <div class="text-4xl font-thin opacity-30 tabular-nums">${newTask.id}</div>
+      <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp"/></div>
+      <div class="list-col-grow">
+        <div><div class="badge badge-warning">${newTask.status}</div>
+</div>
+        <div class="">${newTask.text}</div>
+      </div>
+      <button class="btn btn-square btn-ghost">
+        <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
+      </button>
+    </li>
+  `
+  listContainer.insertAdjacentHTML("beforeend", newTaskHTML)
+  
+  // Repopulate entire localStorage with all todos
+  console.log("JSON.stringify newTask", JSON.stringify(newTask))
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+
+
+  // Clear form
+  addTaskText.value = ""
+
+}
+
+localStorage.getItem("tasks").push(tasks)
+
+
+
+
+
+
+
+//! TASK SCHEMA
+// status: "Active", "Completed", "Deleted"
+// priority: "Urgent", "High", "Normal", "Low"
