4f7ed02091a756c8a31cb3af04a4ce185bb5ee5c
Katherine Tensfeldt <ktensfeldt@perseverenow.org>
2025-09-17 13:00:42 -0700

    Adds dropdown for selecting the priority level when a user is creating a new task. Adds different colors for the badges in the js HTML for the main list. Still need to work on finishing what I am going to do with the status.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 const addTaskButton = document.getElementById("addTaskButton")
 const addTaskText = document.getElementById("addTaskText")
 const listContainer = document.getElementById("listContainer")
-const taskStatusBadge = document.getElementById("task-status-badge")
-
+const taskPriorityBadge = document.getElementById("task-priority-badge")
+const addTaskPriority = document.getElementById("addTaskPriority")
 
 //* INITIALIZE TASK DATA AND GRAB ANY CURRENT TASKS SAVED IN LOCAL STORAGE
 const tasks = JSON.parse(localStorage.getItem("tasks")) || []
-const completedTasks = []
+const archivedTasks = []
 
 //* SVG ICONS
 const editIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="currentColor" viewBox="0 0 640 640"><path d="M100.4 417.2C104.5 402.6 112.2 389.3 123 378.5L304.2 197.3L338.1 163.4C354.7 180 389.4 214.7 442.1 267.4L476 301.3L442.1 335.2L260.9 516.4C250.2 527.1 236.8 534.9 222.2 539L94.4 574.6C86.1 576.9 77.1 574.6 71 568.4C64.9 562.2 62.6 553.3 64.9 545L100.4 417.2zM156 413.5C151.6 418.2 148.4 423.9 146.7 430.1L122.6 517L209.5 492.9C215.9 491.1 221.7 487.8 226.5 483.2L155.9 413.5zM510 267.4C493.4 250.8 458.7 216.1 406 163.4L372 129.5C398.5 103 413.4 88.1 416.9 84.6C430.4 71 448.8 63.4 468 63.4C487.2 63.4 505.6 71 519.1 84.6L554.8 120.3C568.4 133.9 576 152.3 576 171.4C576 190.5 568.4 209 554.8 222.5C551.3 226 536.4 240.9 509.9 267.4z"/></svg>`
@@ -20,13 +20,15 @@ const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="2
 //* BUILD TODO LIST
 const buildTodoList = (task) => {
   console.log("task.status", task.status)
+  console.log("task.priority", task.priority)
+  const badgeColors = task.priority === "Low" ? "badge-info" : task.priority === "Normal" ? "badge-success" : task.priority === "High" ? "badge-warning" : task.priority === "Urgent" ? "badge-error" : "badge-neutral"
  
   return `
       <li id="${task.id}" class="list-row">
         <div id="task-id" class="text-4xl font-thin opacity-30 tabular-nums">${task.id}</div>
         <div class="list-col-grow">
           <div>
-            <div id="task-status-badge" class="badge ${task.status === "Completed" ? "badge-success" : task.status === "Deleted" ? "badge-error" : "badge-warning"}">${task.status}</div>
+            <div id="task-priority-badge" class="badge ${badgeColors}">${task.priority}</div>
           </div>
             <input id="editInput-${task.id}" type="text" placeholder="Edit task..." class="input input-sm hidden" value="${task.text}" />
             <div id="task-text-${task.id}" class="">${task.text}</div>
@@ -57,7 +59,7 @@ if (tasks.length > 0) {
 const handleAddTask = () => {
   console.log("addTaskButton click", addTaskText.value)
   // Create task record
-  const newTask = { id: tasks.length + 1, text: addTaskText.value, status: "Active", priority: "Normal" }
+  const newTask = { id: tasks.length + 1, text: addTaskText.value, priority: addTaskPriority.value, status: "Active" }
   tasks.push(newTask)
   console.log("newTask", newTask, "tasks", tasks)
   
@@ -76,19 +78,22 @@ const handleAddTask = () => {
 }
 
 //* EDIT TASK
-let editButton = ""
+let currentlyEditing = false
+// console.log("first currentlyEditing", currentlyEditing)
 const handleEditTask = (id) => {
   // Get task info
   const taskText = document.getElementById(`task-text-${id}`)
   const editInput = document.getElementById(`editInput-${id}`)
   const taskIndex = tasks.findIndex(task => task.id === id)
 
-  if (editButton === "") {
-    editButton = "edit"
+  if (currentlyEditing === false) {
+    currentlyEditing = true
+    // console.log("second currentlyEditing", currentlyEditing)
     editInput.classList.remove("hidden")
     taskText.classList.add("hidden")
   } else {
-    editButton = ""
+    currentlyEditing = false
+    // console.log("third currentlyEditing", currentlyEditing)
     let newTaskText = editInput.value
     tasks[taskIndex].text = newTaskText
     localStorage.setItem("tasks", JSON.stringify(tasks))
@@ -119,3 +124,17 @@ addTaskText.addEventListener("input", () => {
 //! TASK SCHEMA
 // status: "Active", "Completed", "Deleted"
 // priority: "Urgent", "High", "Normal", "Low"
+
+// SELECT DROPDOWN - badge style
+{/* <select id="edit-status-dropdown-${task.id}" class="badge badge-neutral">
+<option disabled selected value="${task.status}">${task.status}</option>
+<option value="Active">Active</option>
+<option value="Completed">Completed</option>
+<option value="Deleted">Deleted</option>
+</select> */}
+
+// STATUS PING CIRCLE
+{/* <div class="inline-grid *:[grid-area:1/1]">
+  <div class="status status-error animate-ping"></div>
+  <div class="status status-error"></div>
+</div> */}
