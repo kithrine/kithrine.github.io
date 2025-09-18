c5edbf87230dfc39ca7b385a96b49897c043387e
Katherine Tensfeldt <ktensfeldt@perseverenow.org>
2025-09-18 10:37:27 -0700

    Adds completed task functionality when user clicks the button- the task is now pushed into the completedTasks array and removed from the tasks array.

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const listContainer = document.getElementById("listContainer")
 const taskPriorityBadge = document.getElementById("task-priority-badge")
 const addTaskPriority = document.getElementById("addTaskPriority")
 
+
 //* INITIALIZE TASK DATA AND GRAB ANY CURRENT TASKS SAVED IN LOCAL STORAGE
 const tasks = JSON.parse(localStorage.getItem("tasks")) || []
-const archivedTasks = []
+const completedTasks = JSON.parse(localStorage.getItem("completedTasks")) || []
 
 //* SVG ICONS
 const editIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="currentColor" viewBox="0 0 640 640"><path d="M100.4 417.2C104.5 402.6 112.2 389.3 123 378.5L304.2 197.3L338.1 163.4C354.7 180 389.4 214.7 442.1 267.4L476 301.3L442.1 335.2L260.9 516.4C250.2 527.1 236.8 534.9 222.2 539L94.4 574.6C86.1 576.9 77.1 574.6 71 568.4C64.9 562.2 62.6 553.3 64.9 545L100.4 417.2zM156 413.5C151.6 418.2 148.4 423.9 146.7 430.1L122.6 517L209.5 492.9C215.9 491.1 221.7 487.8 226.5 483.2L155.9 413.5zM510 267.4C493.4 250.8 458.7 216.1 406 163.4L372 129.5C398.5 103 413.4 88.1 416.9 84.6C430.4 71 448.8 63.4 468 63.4C487.2 63.4 505.6 71 519.1 84.6L554.8 120.3C568.4 133.9 576 152.3 576 171.4C576 190.5 568.4 209 554.8 222.5C551.3 226 536.4 240.9 509.9 267.4z"/></svg>`
@@ -36,10 +37,10 @@ const buildTodoList = (task) => {
           <button onclick="handleEditTask(${task.id})" class="btn btn-square btn-ghost">
            ${editIcon}
           </button>
-          <button class="btn btn-square btn-ghost">
+          <button onclick="handleCompleteTask(${task.id})" class="btn btn-square btn-ghost">
             ${checkmarkIcon}
           </button>
-      <button class="btn btn-square btn-ghost">
+      <button onclick="handleDeleteTask(${task.id})" class="btn btn-square btn-ghost">
         ${deleteIcon}
       </button>
         </li>
@@ -104,8 +105,34 @@ const handleEditTask = (id) => {
 }
 
 //* DELETE/REMOVE TASK (ARCHIVE)
+const handleDeleteTask = (id) => {
+  let taskItem = document.getElementById(id)
+  let taskIndex = tasks.findIndex(task => task.id === id)
+  tasks.splice(taskIndex, 1)
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+  taskItem.remove()
+}
 
-//* COMPLETE TASK (ARCHIVE)
+let taskCompleteClick = false
+//* COMPLETE TASK
+const handleCompleteTask = (id) => {
+  const taskIndex = tasks.findIndex(task => task.id === id)
+
+  if (taskCompleteClick === false) {
+    console.log("HERE", tasks[taskIndex])
+    tasks[taskIndex].status = "Completed"
+    const completedTask = tasks.splice(taskIndex, 1)[0]
+    console.log("completedTask", completedTask)
+    completedTasks.push(completedTask)
+    console.log("completedTask", completedTask)
+  } else {
+    taskCompleteClick = true
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks))
+  localStorage.setItem("completedTasks", JSON.stringify(completedTasks))
+  console.log("completedTasks", completedTasks)
+
+}
 
 //* DISABLE 'ADD TASK' BUTTON IF NO TEXT IN INPUT FIELD
 addTaskText.addEventListener("input", () => {
