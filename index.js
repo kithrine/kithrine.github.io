fcf16ad7437197fbe2de36dde30bcc11b8714bd5
kithrine <kittensfeldt2@gmail.com>
2026-03-16 14:33:13 -0700

    Removes the complete a task checkmark button. Begins to start doing some of the small styling details like the width of the inputs and buttons.

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const tasks = JSON.parse(localStorage.getItem("tasks")) || []
 //* SVG ICONS
 const editIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="currentColor" viewBox="0 0 640 640"><path d="M100.4 417.2C104.5 402.6 112.2 389.3 123 378.5L304.2 197.3L338.1 163.4C354.7 180 389.4 214.7 442.1 267.4L476 301.3L442.1 335.2L260.9 516.4C250.2 527.1 236.8 534.9 222.2 539L94.4 574.6C86.1 576.9 77.1 574.6 71 568.4C64.9 562.2 62.6 553.3 64.9 545L100.4 417.2zM156 413.5C151.6 418.2 148.4 423.9 146.7 430.1L122.6 517L209.5 492.9C215.9 491.1 221.7 487.8 226.5 483.2L155.9 413.5zM510 267.4C493.4 250.8 458.7 216.1 406 163.4L372 129.5C398.5 103 413.4 88.1 416.9 84.6C430.4 71 448.8 63.4 468 63.4C487.2 63.4 505.6 71 519.1 84.6L554.8 120.3C568.4 133.9 576 152.3 576 171.4C576 190.5 568.4 209 554.8 222.5C551.3 226 536.4 240.9 509.9 267.4z"/></svg>`
 
-const checkmarkIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="currentColor" viewBox="0 0 640 640"><path d="M320 576C178.6 576 64 461.4 64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576zM438 209.7C427.3 201.9 412.3 204.3 404.5 215L285.1 379.2L233 327.1C223.6 317.7 208.4 317.7 199.1 327.1C189.8 336.5 189.7 351.7 199.1 361L271.1 433C276.1 438 282.9 440.5 289.9 440C296.9 439.5 303.3 435.9 307.4 430.2L443.3 243.2C451.1 232.5 448.7 217.5 438 209.7z"/></svg>`
-
 const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="currentColor" viewBox="0 0 640 640"><path d="M232.7 69.9C237.1 56.8 249.3 48 263.1 48L377 48C390.8 48 403 56.8 407.4 69.9L416 96L512 96C529.7 96 544 110.3 544 128C544 145.7 529.7 160 512 160L128 160C110.3 160 96 145.7 96 128C96 110.3 110.3 96 128 96L224 96L232.7 69.9zM128 208L512 208L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 208zM216 272C202.7 272 192 282.7 192 296L192 488C192 501.3 202.7 512 216 512C229.3 512 240 501.3 240 488L240 296C240 282.7 229.3 272 216 272zM320 272C306.7 272 296 282.7 296 296L296 488C296 501.3 306.7 512 320 512C333.3 512 344 501.3 344 488L344 296C344 282.7 333.3 272 320 272zM424 272C410.7 272 400 282.7 400 296L400 488C400 501.3 410.7 512 424 512C437.3 512 448 501.3 448 488L448 296C448 282.7 437.3 272 424 272z"/></svg>`
 
 
@@ -43,7 +41,6 @@ const buildTodoList = (task, index) => {
           </div>
           <div class="task-actions">
           <button onclick="handleEditTask('${task.id}')" class="icon-button" aria-label="edit">${editIcon}</button>
-          <button onclick="handleCompleteTask('${task.id}')" class="icon-button" aria-label="complete">${checkmarkIcon}</button>
           <button onclick="handleDeleteTask('${task.id}')" class="icon-button" aria-label="delete">${deleteIcon}</button>
           </div>
         </li>
@@ -85,7 +82,7 @@ const handleAddTask = () => {
   addTaskPriority.value = "Normal"
   // Re-disable 'Add Task' button
   addTaskButton.setAttribute("disabled", "")
-  buildAllTaskHTMLFunctions()
+  buildTodoListHTML()
 }
 
 //* EDIT TASK
@@ -124,7 +121,7 @@ const handleEditTask = (id) => {
     taskText.classList.remove("hidden")
     editPriorityDropdown.classList.add("hidden")
     priorityBadge.classList.remove("hidden")
-    buildAllTaskHTMLFunctions()
+    buildTodoListHTML()
   }
 }
 
@@ -136,7 +133,7 @@ const handleDeleteTask = (id) => {
   tasks.splice(taskIndex, 1)
   localStorage.setItem("tasks", JSON.stringify(tasks))
   taskItem.remove()
-  buildAllTaskHTMLFunctions()
+  buildTodoListHTML()
 }
 
 //* CLEAR OUT ADD TASK INPUT
@@ -182,7 +179,6 @@ const displayTodoTasks = (tasks) => {
           </div>
           <div class="task-actions">
             <button onclick="handleEditTask('${task.id}')" class="icon-button">${editIcon}</button>
-            <button onclick="handleCompleteTask('${task.id}')" class="icon-button">${checkmarkIcon}</button>
             <button onclick="handleDeleteTask('${task.id}')" class="icon-button">${deleteIcon}</button>
           </div>
       </li>
@@ -190,18 +186,6 @@ const displayTodoTasks = (tasks) => {
     todoListContainer.insertAdjacentHTML("beforeend", taskItem)
   })
 }
-
-
-//! ALL FUNCTIONS FOR BUILDING HTML OF LISTS FOR TASKS!
-const buildAllTaskHTMLFunctions = () => {
-    buildTodoListHTML()
-    buildCompletedListHTML()
-    buildUrgentListHTML()
-    buildHighListHTML()
-    buildNormalListHTML()
-    buildLowListHTML()
-}
-
 
 
 //! THEME TOGGLE
